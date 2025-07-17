b5ed57a92b9a56ff0febbac78d29ed3bda6c5ada
santoshxshrestha <santoshxshrestha@gmail.com>
2025-07-17 17:21:30 +0545

    Added basic routing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{self, App, HttpResponse, HttpServer, Responder, get};
+
+#[get("/")]
+async fn home() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(home))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
