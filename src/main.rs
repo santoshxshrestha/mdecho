e3341ec6ed49804932dd69563771bf2a920b7cbc
santoshxshrestha <santoshxshrestha@gmail.com>
2025-07-18 01:02:13 +0545

    port set

--- src/main.rs
+++ src/main.rs
@@ -51,7 +51,7 @@ async fn main() -> std::io::Result<()> {
             .service(submit_markdown)
             .service(Files::new("/static", "./static"))
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("0.0.0.0", 8080))?
     .run()
     .await
 }
