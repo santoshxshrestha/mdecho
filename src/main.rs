5280f5c67bdfe3102db2c7da8f8902c73fd14d32
santoshxshrestha <santoshxshrestha@gmail.com>
2025-07-18 01:31:23 +0545

    pre tag fix

--- src/main.rs
+++ src/main.rs
@@ -40,7 +40,7 @@ async fn submit_markdown(form: Form<FormData>) -> impl Responder {
 
     HttpResponse::Ok()
         .content_type("text/html")
-        .body(format!("<pre>{}<pre>", html_output))
+        .body(format!("<pre class=\"pre-tag\">{}<pre>", html_output))
 }
 
 #[actix_web::main]
