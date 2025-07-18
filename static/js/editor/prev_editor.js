b193248fbd603d4260d0ad7c53e412ab999cb479
santoshxshrestha <santoshxshrestha@gmail.com>
2025-07-19 00:53:38 +0545

    unused var and line height fix

--- static/js/editor/prev_editor.js
+++ static/js/editor/prev_editor.js
@@ -1,11 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const savedEditor = localStorage.getItem("editor");
-    const html = document.documentElement;
-    const button = document.querySelector("#toggleVimMode");
-
-    if (savedEditor === "vscode") {
-       vimModeEnabled  = true;
-    } else if(savedEditor === "vim") 
-       vimModeEnabled  = false;
-    }
+   const savedEditor = localStorage.getItem("editor");
+   if (savedEditor === "vscode") {
+      vimModeEnabled = true;
+   } else if (savedEditor === "vim") {
+      vimModeEnabled = false;
+   }
 });
